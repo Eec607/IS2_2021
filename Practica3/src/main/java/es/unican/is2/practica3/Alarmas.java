6d60482d202c5b10357ee18c0f2c5d2b9d0a49a6
ara998 <ara998@alumnos.unican.es>
2021-03-25 13:19:42 +0100

    alarmas y alarmasState creados

--- Practica3/src/main/java/es/unican/is2/practica3/Alarmas.java
+++ Practica3/src/main/java/es/unican/is2/practica3/Alarmas.java
@@ -1,7 +1,36 @@
 package es.unican.is2.practica3;
 
+import java.util.Date;
+
 public class Alarmas {
 	
-	private AlarmasState
-
+	private AlarmasState state;
+	
+	public Alarmas() {
+		state = AlarmasState.init(this);
+	}
+	
+	public void setState(AlarmasState value) {
+		this.state = value;
+	}
+	
+	public void alarmaOn(String id) {
+		state.alarmaOn(this, id);
+	}
+	
+	public void alarmaOff(String id) {
+		state.alarmaOff(this, id);
+	}
+		
+	public void nuevaAlarma(String id, Date hora) {
+		state.nuevaAlarma(this, id, hora);
+	}
+	
+	public void borraAlarma(String id) {
+		state.borraAlarma(this, id);
+	}
+	
+	public void apagar() {
+		state.apagar(this);
+	}
 }
--- Practica3/src/main/java/es/unican/is2/practica3/AlarmasState.java
+++ Practica3/src/main/java/es/unican/is2/practica3/AlarmasState.java
@@ -1,11 +1,34 @@
 package es.unican.is2.practica3;
 
+import java.util.Date;
+
 public abstract class AlarmasState {
 	
 	private static Desprogramado estadoDesprogramado = new Desprogramado();
 	private static Programado estadoProgramado = new Programado();
 	private static Sonando estadoSonando = new Sonando();
 	
-	public static AlarmasState init()
+	public static AlarmasState init(Alarmas context) {
+		estadoDesprogramado.entryAction(context);
+		return estadoDesprogramado;
+	}
+	
+	public void alarmaOn(Alarmas context, String id) {};
+	public void alarmaOff(Alarmas context, String id) {};
+	public void nuevaAlarma(Alarmas context, String id, Date hora) {};
+	public void borraAlarma(Alarmas context, String id) {};
+	public void apagar(Alarmas context) {};
+	
+	public static AlarmasState getEstadoDesprogramado() {
+		return estadoDesprogramado;
+	};
+	public static AlarmasState getEstadoProgramado() {
+		return estadoProgramado;
+	};
+	public static AlarmasState getEstadoSonando() {
+		return estadoSonando;
+	};
+	
+	
 
 }
